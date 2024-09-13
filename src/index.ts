792e0e35a4e0ed35bb1108ecc3b726bc1f7b54c5
edwinliiiii <91173669+edwinliiiii@users.noreply.github.com>
2024-09-13 14:39:48 -0400

    added subtract method + test

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-import { Calculator } from './calculator';
+import { Calculator } from "./calculator";
 
 const calc = new Calculator();
 
 const result_add = calc.add(2, 3);
 console.log(`result_add = ${result_add}`);
 
+const result_sub = calc.subtract(2, 3);
+console.log(`result_add = ${result_sub}`);
