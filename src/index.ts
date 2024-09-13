5003e241b7797ea2c7ff365cc82ed782b7d8b386
edwinliiiii <91173669+edwinliiiii@users.noreply.github.com>
2024-09-13 14:48:29 -0400

    added multiplication operator and test

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,6 @@ console.log(`result_add = ${result_add}`);
 
 const result_sub = calc.subtract(2, 3);
 console.log(`result_add = ${result_sub}`);
+
+const result_mult = calc.multiply(2, 3);
+console.log(`result_add = ${result_mult}`);
